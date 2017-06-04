ca683b6463299954469e0939d73a876a56a94066
cnneillee <cn.neillee@gmail.com>
2017-06-04 20:27:24 +0800

    change way of getting bugly appid

--- app/src/main/java/cn/neillee/dailyzhijiu/utils/AppUtil.java
+++ app/src/main/java/cn/neillee/dailyzhijiu/utils/AppUtil.java
@@ -6,6 +6,7 @@ import android.content.ClipboardManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -18,10 +19,6 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
-import cn.neillee.dailyzhijiu.Constant;
-import com.neil.dailyzhijiu.R;
-import cn.neillee.dailyzhijiu.app.DailyApp;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -29,6 +26,10 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Locale;
+
+import cn.neillee.dailyzhijiu.Constant;
+import cn.neillee.dailyzhijiu.R;
+import cn.neillee.dailyzhijiu.app.DailyApp;
 
 /**
  * 作者：Neil on 2017/3/5 17:34.
@@ -56,6 +57,34 @@ public class AppUtil {
             e.printStackTrace();
         }
         return versionCode;
+    }
+
+    /**
+     * 获取application中指定的meta-data
+     *
+     * @return 如果没有获取成功(没有对应值, 或者异常)，则返回值为空
+     */
+    public static String getAppMetaData(Context ctx, String key) {
+        if (ctx == null || TextUtils.isEmpty(key)) {
+            return null;
+        }
+        String resultData = null;
+        try {
+            PackageManager packageManager = ctx.getPackageManager();
+            if (packageManager != null) {
+                ApplicationInfo applicationInfo = packageManager.getApplicationInfo(ctx.getPackageName(), PackageManager.GET_META_DATA);
+                if (applicationInfo != null) {
+                    if (applicationInfo.metaData != null) {
+                        resultData = applicationInfo.metaData.getString(key);
+                    }
+                }
+
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return resultData;
     }
 
     /**
